1e29d496fc8a0ee956420acee87bd16a1f282fbd
ProofStrik <ttgyigit@gmail.com>
2024-05-26 01:52:48 -0800

    Update index.js
    king

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 var prefix = "+";//istediğiniz gibi ayarlayabilirsiniz prefixi
 var statuses = [`Altyapı Videosu İçin youtube.com/@WraithsDev`];
 var timers = 2;
-const owners = ["sahip_id"];//botu kullanmak için sahip_id girmeyi unutmayın
+const owners = ["1062811826213105715"];//botu kullanmak için sahip_id girmeyi unutmayın
 
 client.on("ready", () => {
   console.log(`Giriş Yapıldı: ${client.user.tag}`);
@@ -157,4 +157,4 @@ client.on("message", async message => {
   }
 });
 
-client.login(`bottokengir`);
+client.login(`MTIzMTk2NzQ0MzA4MjAyMjkxMg.GcDps4.deawKAqElqvpWWCqstvW6x8LfCglOHQA0h8Yfs`);
